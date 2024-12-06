fb0139894abf4b731fd077e7672120514bbb137b
nubiabogale <nubiabogale07@gmail.com>
2024-12-05 20:46:20 -0500

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-// string for user
+// 1. Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+// 2. Assign three variables. Each variable contains the corresponding result of calculation using a unique arithmetic operator(+-*/). Each must equal one of the three codes in the combination.
+// 3. Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog.
+
+
+
+// 1. string for user
 const str =
   "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
 
-// assigning each variable with unique arithmetic operator:
+// 2. assigning each variable with unique arithmetic operator:
 let num1 = 5 + 5;
 let num2 = 10 * 4;
 let num3 = 40 - 1;
 
-//console.log(num1, num2, num3) to check
-
+// 3. dialogue box
 alert(`${str} ${num1} ${num2} ${num3}`);
-//dialogue box^
