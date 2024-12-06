1e37c8c903d2be6df4906e865cab5aee3f1a51aa
nubiabogale <nubiabogale07@gmail.com>
2024-12-05 19:30:09 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// string for user
+const str =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// assigning each variable with unique arithmetic operator:
+let num1 = 5 + 5;
+let num2 = 10 * 4;
+let num3 = 40 - 1;
+
+//console.log(num1, num2, num3) to check
+
+alert(`${str} ${num1} ${num2} ${num3}`);
+//dialogue box^
